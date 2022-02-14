52a630f568f3d5b7b2dde825b2dca588da4a5947
Rap20092003 <99651660+Rap20092003@users.noreply.github.com>
2022-02-14 16:53:02 +0530

    Update Wordlister.py

--- Wordlister.py
+++ Wordlister.py
@@ -14,7 +14,7 @@ L = r.randint(0, 26)
 C = r.randint(0, 6)
 S = r.randint(0, 2)
 result = pyfiglet.figlet_format("Wordlister", font = List[L]  )
-result = colored(result, Color[C] , attrs=['reverse'])
+result = colored(result, Color[C] , attrs=[Style[S]])
 print(result)
 
 x = int(input("Minimum Number: "))
